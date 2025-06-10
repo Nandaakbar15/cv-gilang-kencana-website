b34af244d24b9b01072741f633fe28f0b008a933
Alvinda_akbar10 <alvindaakbar@gmail.com>
2025-06-10 14:49:39 +0700

    Revisi halaman portfolio

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Kontraktor;
 use App\Models\Contact;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+
 
 class UserController extends Controller
 {
@@ -34,8 +38,170 @@ class UserController extends Controller
         ]);
     }
 
-    public function portofolio()
+    public function portofolio(Request $request)
     {
-        return view("dashboard.portofolio.viewportofolio");
+        $portfolios = [
+            [
+                'judul' => 'Eksterior Rumah',
+                'gambar' => 'images/Eksterior Rumah.jpg'
+            ],
+            [
+                'judul' => 'Meeting Room',
+                'gambar' => 'images/Meeting Room.jpg'
+            ],
+            [
+                'judul' => 'Ruang Manager',
+                'gambar' => "images/Ruang Manager.jpg"
+            ],
+            [
+                'judul' => 'Kitchen Set',
+                'gambar' => 'images/Kitchen Set.jpg'
+            ],
+            [
+                'judul' => 'Kimia Farma Apotek Unit Bisnis Bekasi',
+                'gambar' => [
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi1.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi2.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi3.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi4.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi5.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi6.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi7.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi8.png',
+                    'images/Foto Dokumentasi Pekerjaan KF Bekasi/kfbekasi9.png',
+                ],
+            ],
+            [
+                    'judul' => 'Kimia Farma Apotek Unit Bisnis Balikpapan',
+                    'gambar' => [
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan1.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan2.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan3.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan4.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan5.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan6.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan7.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan8.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Balikpapan/kfbalikpapan9.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Kimia Farma Apotek Bisnis Batam 1',
+                    'gambar' => [
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image001.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image002.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image003.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image004.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image005.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image006.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image007.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 1/image008.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Kimia Farma Apotek Bisnis Batam 2',
+                    'gambar' => [
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image001.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image002.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image003.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image004.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image005.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image006.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image007.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Batam 2/image008.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Kimia Farma Apotek Diagnostika Unit Jabar 5',
+                    'gambar' => [
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image001.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image002.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image003.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image004.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image005.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image006.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image007.png',
+                        'images/Foto Dokumentasi Pekerjaan Diagnostika Unit Jabar 5/image008.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Kimia Farma Apotek Bisnis Cirebon',
+                    'gambar' => [
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image001.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image002.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image003.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image004.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image005.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image006.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image007.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Cirebon/image008.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Kimia Farma Apotek Bisnis Tasikmalaya',
+                    'gambar' => [
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image001.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image002.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image003.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image004.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image005.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image006.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image007.png',
+                        'images/Foto Dokumentasi Pekerjaan KF Tasikmalaya/image008.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Rumah Tinggal Grand Terra Cimahi',
+                    'gambar' => 'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Grand Terra Cimahi/image001.png'
+                ],
+                [
+                    'judul' => 'Rumah Tinggal Bapak Bapak Ferry Kota Batam',
+                    'gambar' => 'images/Foto Pekerjaan Non PT. Kimia Farma/RUMAH TINGGAL BAPAK FERRY KOTA BATAM/image001.jpg'
+                ],
+                [
+                    'judul' => 'Rumah Tinggal Bapak Nurcahyo Cluster Kota Wisata',
+                    'gambar' => [
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Bapak Nurcahyo Cluster Miami Kota Wisata/image001.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Bapak Nurcahyo Cluster Miami Kota Wisata/image002.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Bapak Nurcahyo Cluster Miami Kota Wisata/image003.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Bapak Nurcahyo Cluster Miami Kota Wisata/image004.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Bapak Nurcahyo Cluster Miami Kota Wisata/image005.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Rumah Tinggal Bapak Nurcahyo Cluster Miami Kota Wisata/image006.png',
+                    ]
+                ],
+                [
+                    'judul' => 'Wakaf Pembangunan Masjid AL Istiqomah Ciawitali RW 10 Citereup',
+                    'gambar' => [
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image001.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image002.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image003.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image004.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image005.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image006.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image007.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image008.png',
+                        'images/Foto Pekerjaan Non PT. Kimia Farma/Wakaf Pembangunan Masjid AL ISTIQOMAH Ciawitali RW 10 Citererup Kota/image009.png',
+                    ]
+                ],
+        ];
+
+        $collection = collect($portfolios);
+
+        // Pagination
+        $perPage = 4;
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $currentItems = $collection->slice(($currentPage - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator(
+            $currentItems,
+            $collection->count(),
+            $perPage,
+            $currentPage,
+            ['path' => $request->url(), 'query' => $request->query()]
+        );
+
+        return view("dashboard.portofolio.viewportofolio", [
+            'paginated' => $paginated
+        ]);
     }
 }
--- resources/views/dashboard/portofolio/viewportofolio.blade.php
+++ resources/views/dashboard/portofolio/viewportofolio.blade.php
@@ -19,39 +19,31 @@
             @include('dashboard.partials.navbar')
             <!-- Page Content-->
             <section class="py-5">
-                <div class="container px-5 my-5">
-                    <div class="text-center mb-5">
-                        <h1 class="fw-bolder">Our Work</h1>
-                        <p class="lead fw-normal text-muted mb-0">Portofolio CV Gilang Kencana Mulya</p>
-                    </div>
-                    <div class="row gx-5">
-                        <div class="col-lg-6">
-                            <div class="position-relative mb-5">
-                                <img class="img-fluid rounded-3 mb-3" src="{{ asset("images/Eksterior rumah.jpg") }}" alt="..." />
-                                <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Eksterior Rumah</a>
-                            </div>
+                        <div class="container px-5 my-5">
+                            @foreach ($paginated as $item)
+                                <div class="text-center mb-4 mt-5">
+                            <h3 class="fw-bold">{{ $item['judul'] }}</h3>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="position-relative mb-5">
-                                <img class="img-fluid rounded-3 mb-3" src="{{ asset("images/Meeting Room.jpg") }}" alt="..." />
-                                <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Meeting Room</a>
-                            </div>
+
+                        <div class="row gx-5 gy-4">
+                            @if(is_array($item['gambar']))
+                                @foreach($item['gambar'] as $gambar)
+                                    <div class="col-lg-4 col-md-6">
+                                        <img class="img-fluid rounded-3 shadow-sm" src="{{ asset($gambar) }}" alt="{{ $item['judul'] }}" style="height: 250px;">
+                                    </div>
+                                @endforeach
+                            @else
+                                <div class="col-lg-4 col-md-6 offset-lg-4">
+                                    <img class="img-fluid rounded-3 shadow-sm" src="{{ asset($item['gambar']) }}" alt="{{ $item['judul'] }}" style="height: 250px;">
+                                </div>
+                            @endif
                         </div>
-                        <div class="col-lg-6">
-                            <div class="position-relative mb-5 mb-lg-0">
-                                <img class="img-fluid rounded-3 mb-3" src="{{ asset("images/Ruang Manager.jpg") }}" alt="..." />
-                                <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Ruang Manager</a>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="position-relative">
-                                <img class="img-fluid rounded-3 mb-3" src="{{ asset("images/Kitchen Set.jpg") }}" alt="..." />
-                                <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Kitchen Set</a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </section>
+            <div class="container my-4 d-flex justify-content-center">
+                {{ $paginated->links('pagination::bootstrap-5') }}
+            </div>
             <section class="py-5 bg-light">
                 <div class="container px-5 my-5">
                     <h2 class="display-4 fw-bolder mb-4">Let's build something together</h2>
